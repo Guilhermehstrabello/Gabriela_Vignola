e884b946a6f59e1620d93d2da209987fc429015e
Guilhermehstrabello <gui.strabehonorio@outlook.com>
2024-09-16 20:26:33 -0300

    Adicionei a Clarity na LP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Script from "next/script"; 
 
 export const metadata = {
   title: "Manual UGC",
@@ -19,6 +20,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+      <Script id="microsoft-clarity-analytics">
+  {`
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "[o4btkfm6zk]");
+  `}
+</Script>
+      </head>
       <body className={montserrat.className}>{children}</body>
     </html>
   );
