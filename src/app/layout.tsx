2d300943da9ef50c94894a5fd904213ff048c53a
Guilhermehstrabello <gui.strabehonorio@outlook.com>
2024-09-17 16:15:18 -0300

    resolvendo erros

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-<<<<<<< HEAD
 import Script from "next/script";
-=======
-import Script from "next/script"; 
->>>>>>> e884b946a6f59e1620d93d2da209987fc429015e
 
 export const metadata = {
   title: "Manual UGC",
@@ -24,17 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <head>
-      <Script id="microsoft-clarity-analytics">
-  {`
-      (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "[o4btkfm6zk]");
-  `}
-</Script>
-      </head>
       <body className={montserrat.className}>{children}</body>
       <Script id="microsoft-clarity-analytics">
         {`
@@ -42,7 +27,7 @@ export default function RootLayout({
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "[YOUR-ID-HERE]");
+        })(window, document, "clarity", "script", "[o4btkfm6zk]");
     `}
       </Script>
     </html>
